b29440b69b38d355647b0dff0ce6ad92128e07e8
danjinming <danjinming@gmail.com>
2013-05-05 01:11:24 +0800

    BlackWhite Work

--- EPaper/EPViewController.m
+++ EPaper/EPViewController.m
@@ -9,6 +9,9 @@
 #import "EPViewController.h"
 #import "EPFlatButton.h"
 #import "EPCaptureViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <QuartzCore/QuartzCore.h>
+#import <CoreGraphics/CGColorSpace.h>
 
 @interface EPViewController ()
 
@@ -133,6 +136,155 @@
 - (void)onView
 {
     NSLog(@"onView\n");
+}
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    CGSize size = {0};
+    UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+    size = [image size];
+    //NSData *imageData = NULL;
+    //imageData = UIImagePNGRepresentation(image);
+    struct CGImage *inImage = [image CGImage];// initWithCGImage]:
+    //struct CGImage *bitmap_bw = CGImageCreateCopy(bitmap);
+    //size_t w = CGImageGetWidth(bitmap_bw);
+    //size_t h = CGImageGetHeight(bitmap_bw);
+    //NSLog(@"ow = %f, oh = %f\n", size.width, size.height);
+    //NSLog(@"xw = %zd, xh = %zd, bpp = %zd \n", w, h, CGImageGetBitsPerPixel(bitmap_bw));
+    
+    CGContextRef cgctx = [self CreateARGBBitmapContext:inImage];
+    if (cgctx == NULL)
+    {
+        return;
+    }
+    
+    size_t w = CGImageGetWidth(inImage);
+    size_t h = CGImageGetHeight(inImage);
+    CGRect rect = {{0,0},{w,h}};
+    
+    CGContextDrawImage(cgctx, rect, inImage);
+    void *data = CGBitmapContextGetData (cgctx);
+    if (data != NULL){
+        [self gray:data withWidth:w withHeight:h]; //Gray
+    }
+    
+    CGImageRef image_bw = CGBitmapContextCreateImage(cgctx);
+    [self saveRGB:image_bw];
+    
+    CGContextRelease(cgctx);
+    if (data){
+        free(data);
+    }
+}
+
+- (CGContextRef)CreateARGBBitmapContext:(struct CGImage *)inImage
+{
+    CGContextRef    context = NULL;
+    CGColorSpaceRef colorSpace;
+    void *          bitmapData;
+    int             bitmapByteCount;
+    int             bitmapBytesPerRow;
+    
+    // Get image width, height. We'll use the entire image.
+    size_t pixelsWide = CGImageGetWidth(inImage);
+    size_t pixelsHigh = CGImageGetHeight(inImage);
+    
+    // Declare the number of bytes per row. Each pixel in the bitmap in this
+    // example is represented by 4 bytes; 8 bits each of red, green, blue, and
+    // alpha.
+    bitmapBytesPerRow   = (pixelsWide * 4);
+    bitmapByteCount     = (bitmapBytesPerRow * pixelsHigh);
+    
+    // Use the generic RGB color space.
+    //colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
+    colorSpace = CGColorSpaceCreateDeviceRGB();
+    if (colorSpace == NULL)
+    {
+        fprintf(stderr, "Error allocating color space\n");
+        return NULL;
+    }
+    
+    // Allocate memory for image data. This is the destination in memory
+    // where any drawing to the bitmap context will be rendered.
+    bitmapData = malloc( bitmapByteCount );
+    if (bitmapData == NULL)
+    {
+        fprintf (stderr, "Memory not allocated!");
+        CGColorSpaceRelease( colorSpace );
+        return NULL;
+    }
+    
+    // Create the bitmap context. We want pre-multiplied ARGB, 8-bits
+    // per component. Regardless of what the source image format is
+    // (CMYK, Grayscale, and so on) it will be converted over to the format
+    // specified here by CGBitmapContextCreate.
+    context = CGBitmapContextCreate (bitmapData,
+                                     pixelsWide,
+                                     pixelsHigh,
+                                     8,      // bits per component
+                                     bitmapBytesPerRow,
+                                     colorSpace,
+                                     kCGImageAlphaPremultipliedFirst);
+    if (context == NULL)
+    {
+        free (bitmapData);
+        fprintf (stderr, "Context not created!");
+    }
+    
+    // Make sure and release colorspace before returning
+    CGColorSpaceRelease( colorSpace );
+    
+    return context;
+}
+
+
+- (void)saveRGB:(CGImageRef)image
+{
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    [library writeImageToSavedPhotosAlbum:image
+                              orientation:UIImageOrientationUp
+                          completionBlock:^(NSURL *assetURL, NSError *error){
+                              if (error) {
+                                  UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error Saving"
+                                                                                  message:[error localizedDescription]
+                                                                                 delegate:nil
+                                                                        cancelButtonTitle:@"Ok"
+                                                                        otherButtonTitles: nil];
+                                  [alert show];
+                              }
+                          }];
+}
+
+- (void)gray:(char*)head withWidth:(size_t)_width withHeight:(size_t)_height
+{
+    tracepoint();
+    int sum;
+    PRGB32  ptr;
+    
+    ptr = (PRGB32)head;
+    for(int i=0; i<_width; i++) {
+        for(int j=0; j<_height; j++) {
+            sum = 0;
+            sum += ptr->r;
+            sum += ptr->g;
+            sum += ptr->b;
+            //sum += ptr->a;
+            
+            if(sum > 350) {
+                ptr->r = 0xff;
+                ptr->g = 0xff;
+                ptr->b = 0xff;
+                //ptr->a = 0xff;
+            } else {
+                ptr->r = 0;
+                ptr->g = 0;
+                ptr->b = 0;
+                //ptr->a = 0xff;
+            }
+            ptr++;
+        }
+    }
 }
 
 @end
